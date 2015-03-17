390f0fe4bf453a8749d941677cc028fa1ab2e4d9
Neel Patil <neelvp@gmail.com>
2015-03-17 05:30:15 +0000

    mailing list added

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@
   #  root to: redirect('/about.html') end
   Rails.application.routes.draw do 
     resources :contacts, only: [:new, :create]
+    resources :visitors, only: [:new, :create]
     root to: 'visitors#new' 
   end
 
